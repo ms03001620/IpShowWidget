0ca53b62c7c89a84387300d350515ecd15640381
Mark <mazhenjin@sina.com>
2016-01-06 22:44:47 +0800

    async get ip

--- app/src/main/java/org/mark/ipshow/IpUtils.java
+++ app/src/main/java/org/mark/ipshow/IpUtils.java
@@ -4,6 +4,12 @@ import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.text.format.Formatter;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 /**
  * Created by Mark on 2016/1/4.
  */
@@ -14,4 +20,24 @@ public class IpUtils {
         String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
         return ip;
     }
+
+    public static String getLocalIpAddress(){
+        try{
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf
+                        .getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        }catch (SocketException e) {
+            return e.getMessage();
+        }
+
+        return "No connection";
+    }
+
 }
--- app/src/main/java/org/mark/ipshow/IpWidgetProvider.java
+++ app/src/main/java/org/mark/ipshow/IpWidgetProvider.java
@@ -6,27 +6,43 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
  * Created by Mark on 2016/1/4.
  */
 public class IpWidgetProvider extends AppWidgetProvider {
-    RemoteViews remoteViews;
-    AppWidgetManager appWidgetManager;
-    ComponentName thisWidget;
+    private static final String TAG = "IpWidgetProvider";
     private static final String MyOnClick = "myOnClickTag";
+
+    private Context mContext;
+    Handler mHandler;
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        this.appWidgetManager = appWidgetManager;
-        remoteViews =  new RemoteViews(context.getPackageName(), R.layout.widget_main);
-        thisWidget = new ComponentName(context, IpWidgetProvider.class);
-
-        remoteViews.setTextViewText(R.id.text_ip, IpUtils.getIpV4String(context));
-
+        mContext = context;
+        RemoteViews remoteViews =  new RemoteViews(context.getPackageName(), R.layout.widget_main);
+        ComponentName thisWidget = new ComponentName(context, IpWidgetProvider.class);
         remoteViews.setOnClickPendingIntent(R.id.btn_refresh, getPendingSelfIntent(context, MyOnClick));
-
         appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+
+        mHandler = new Handler(context.getMainLooper(), new Handler.Callback() {
+            public boolean handleMessage(Message message) {
+                RemoteViews remoteViews =  new RemoteViews(mContext.getPackageName(), R.layout.widget_main);
+                remoteViews.setTextViewText(R.id.text_ip, String.valueOf(message.obj));
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(mContext);
+                ComponentName thisWidget = new ComponentName(mContext, IpWidgetProvider.class);
+                appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+                return false;
+            }
+        });
+
+        Log.d(TAG, "onUpdate");
+        new Thread(networkTask).start();
     }
 
     protected PendingIntent getPendingSelfIntent(Context context, String action) {
@@ -36,15 +52,23 @@ public class IpWidgetProvider extends AppWidgetProvider {
     }
 
     public void onReceive(Context context, Intent intent) {
-
         if (MyOnClick.equals(intent.getAction())){
-            RemoteViews remoteViews =  new RemoteViews(context.getPackageName(), R.layout.widget_main);
-            remoteViews.setTextViewText(R.id.text_ip, IpUtils.getIpV4String(context));
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            ComponentName thisWidget = new ComponentName(context, IpWidgetProvider.class);
-            appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+            Log.d(TAG, "MyOnClick");
+            mContext = context;
+            new Thread(networkTask).start();
         }
-
         super.onReceive(context, intent);
     };
+
+    Runnable networkTask = new Runnable() {
+        @Override
+        public void run() {
+            Message msg = new Message();
+            String ip = IpUtils.getLocalIpAddress();
+            Log.d(TAG, ip);
+            msg.obj = ip;
+            mHandler.sendMessage(msg);
+        }
+    };
+
 }
